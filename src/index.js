94d1f18353fc6c98add5d0aa1a8d707ac65f57d0
YuPashintseva <juliyapashintseva@gmail.com>
2020-10-14 22:23:54 +0600

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let map = new Map();
+    var stack = [];
+    var openBr = [];
+    var closeBr = [];
+    for (let i=0; i<bracketsConfig.length; i+=1) {
+      map.set(bracketsConfig[i][1],bracketsConfig[i][0]);
+      openBr.push(bracketsConfig[i][0]);
+      closeBr.push(bracketsConfig[i][1]);
+    }
+    for(let s=0; s<str.length; s+=1) {
+      if (openBr.indexOf(str[s]) !== -1 && closeBr.indexOf(str[s]) !== -1) {
+        if (stack[stack.length-1] === str[s]) {
+          stack.pop();
+        } else {
+          stack.push(str[s]);
+        }
+      } else {
+        if (openBr.indexOf(str[s]) !== -1) {
+          stack.push(str[s]);
+        } else {
+          var lastel = stack[stack.length-1];
+          if (map.get(str[s]) === lastel) {
+
+            stack.pop();
+          } else {
+            return false;
+          }
+          
+        }
+      }
+    }
+  
+    if (stack.length === 0) {
+      return true;
+    }
+    return false;
+    
+  }
+
+
